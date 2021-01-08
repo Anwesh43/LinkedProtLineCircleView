5fee9cf34948b05ab3ec3a3dc2f9323291a6ddc2
Anwesh43 <anweshthecool0@gmail.com>
2021-01-09 03:55:23 +0530

    adding ProtLineCircleView class

--- protlinecircleview/src/main/java/com/example/protlinecircleview/ProtLineCircleView.kt
+++ protlinecircleview/src/main/java/com/example/protlinecircleview/ProtLineCircleView.kt
@@ -66,3 +66,18 @@ fun Canvas.draPLCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawProtLineCircle(scale, w, h, paint)
 }
+
+class ProtLineCircleView(ctx : Context) : View(ctx) {
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
