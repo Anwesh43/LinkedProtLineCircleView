c526734fcdb1fdbe57d35ec38cc4eacc692e335c
Anwesh43 <anweshthecool0@gmail.com>
2021-01-09 03:53:21 +0530

    adding Canvas extension methods

--- protlinecircleview/src/main/java/com/example/protlinecircleview/ProtLineCircleView.kt
+++ protlinecircleview/src/main/java/com/example/protlinecircleview/ProtLineCircleView.kt
@@ -31,3 +31,38 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawProtLineCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    paint.style = Paint.Style.STROKE
+    val c : Float = Math.min(w, h) / cFactor
+    val l1 : Float = Math.min(w, h) / l1Factor
+    val l2 : Float = Math.min(w, h) / l2Factor
+    save()
+    translate(w / 2, h / 2)
+    drawArc(RectF(-c, -c, c, c), -90f, 360f * sf1, false, paint)
+    for (j in 0..1) {
+        save()
+        rotate(45f * (1f - 2 * j) * sf4)
+        drawLine(0f, 0f, 0f, -l1 * sf2, paint)
+        restore()
+        save()
+        rotate(45f * (1f - 2 * j) * sf3)
+        drawLine(0f, 0f, 0f, l2 * sf2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.draPLCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawProtLineCircle(scale, w, h, paint)
+}
